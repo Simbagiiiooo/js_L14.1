75066b6dc8b8bf0d12c7af6d5aa20329e651a9b1
Simbagiiiooo <67731418+Simbagiiiooo@users.noreply.github.com>
2021-02-16 19:37:13 +0100

    Add files via upload

---
+++ js_L14.1/script.js
@@ -0,0 +1,22 @@
+const makePizza = function() {
+console.log("Rol the bottom");
+console.log("Put tomato sauce on");
+console.log("Put meat on");
+console.log("Put vegetables on");
+console.log("Put in oven");
+};
+
+const makeSushi = function() {
+console.log("Kill the fish");
+console.log("boil the rice");
+console.log("Put salt to the fish");
+console.log("Put salt to the rice");
+console.log("Put rice and fish together");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
